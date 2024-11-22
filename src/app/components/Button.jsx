f2e509957857958678462c049e01e93f676c29fb
Victor Schweitz <victorschweitz@hotmail.com>
2024-11-22 11:59:17 +0100

    Set up Button component

--- src/app/components/Button.jsx
+++ src/app/components/Button.jsx
@@ -1,7 +1,15 @@
-const Button = () =>
+'use client'
+import Link from 'next/link'
+
+const Button = ({ href, className, buttonText }) =>
 {
     return (
-       <></>
+            <Link
+                href={href}
+                className="bg-green-500 text-white px-6 py-3 flex justify-center min-w-fit max-w-full uppercase font-bold text-lg tracking-wider rounded-lg transition ease-in duration-150 hover:opacity-80"
+            >
+                {buttonText}
+            </Link>
     )
 }
 
---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import Button from './components/Button.jsx'
+
+export default function Page() 
+{
+    return (
+        <>
+            <Button href="#" buttonText="Hey" />
+        </>
+    ) 
+}
