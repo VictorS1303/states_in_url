b10327cf9ab25e45fd03027d78f788670a316080
Victor Schweitz <victorschweitz@hotmail.com>
2024-11-23 09:54:45 +0100

    Fixed SizePicker UI

--- src/app/components/SizePicker.jsx
+++ src/app/components/SizePicker.jsx
@@ -1,14 +1,14 @@
-import Button from './Button.jsx'
+import Link from 'next/link'
 import Header from './Header.jsx'
 
-const SizePicker = ({ headerText }) =>
+const SizePicker = () =>
 {
     const sizes = ['xs', 's', 'm', 'lg', 'xl', 'xxl']
 
     return (
         <article className="mt-4">
-            <Header headerText={headerText}>
-                <h3 headerText="Size" className="text-black text-xl text-left">Size</h3>
+            <Header>
+                <h3 className="text-black text-xl text-left">Size</h3>
             </Header>
 
             <div className="container size-buttons-container flex gap-4 mt-2">
@@ -16,9 +16,9 @@ const SizePicker = ({ headerText }) =>
                     sizes.map((size, index) =>
                     {
                         return (
-                            <button key={index} className="bg-green-500 w-[50px] h-[50px] text-lg uppercase rounded-md cursor-pointer transition ease-in duration-150 hover:opacity-80">
+                            <Link key={index} href="#" className="grid place-content-center bg-green-500 w-[50px] h-[50px] text-lg uppercase rounded-md cursor-pointer transition ease-in duration-150 hover:opacity-80">
                                 {size}
-                            </button>
+                            </Link>
                         )
 
                     })
